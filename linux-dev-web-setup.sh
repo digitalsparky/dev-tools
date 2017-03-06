875139cb8a0484587c3a4e027b61135ba7f1f2f1
Matt Spurrier <matthew@spurrier.com.au>
2017-03-06 15:23:36 +0800

    Update linux-dev-web-setup.sh

--- linux-dev-web-setup.sh
+++ linux-dev-web-setup.sh
@@ -52,11 +52,7 @@ sudo apt-get install -qy curl linux-image-extra-$(uname -r) linux-image-extra-vi
 sudo add-apt-repository ppa:nginx/stable
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${UBUNTU_CODENAME} stable"
 
 sudo apt-get -q update > /dev/null 2>&1
 sudo apt-get -q -y upgrade > /dev/null 2>&1
